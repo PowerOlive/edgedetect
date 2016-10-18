788ccc372a24153ce051f245724cdcb9f3a99530
Ox Cart <ox.to.a.cart@gmail.com>
2016-10-18 16:18:08 -0500

    Looking up Edge software name from registry

--- edgedetect_windows.go
+++ edgedetect_windows.go
@@ -9,7 +9,7 @@ import (
 func defaultBrowserIsEdge() bool {
 	k, err := registry.OpenKey(registry.CLASSES_ROOT, `ActivatableClasses\Package\DefaultBrowser_NOPUBLISHERID\Server\DefaultBrowserServer`, registry.QUERY_VALUE)
 	if err != nil {
-		log.Errorf("Error reading DefaultBrowserServer registry key: %v", err)
+		log.Debugf("Error reading DefaultBrowserServer registry key: %v", err)
 		return checkURLAssociations()
 	}
 	defer k.Close()
@@ -25,17 +25,27 @@ func defaultBrowserIsEdge() bool {
 }
 
 func checkURLAssociations() bool {
-	k, regErr := registry.OpenKey(registry.CURRENT_USER, `Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice`, registry.QUERY_VALUE)
+	_p, regErr := registry.OpenKey(registry.CURRENT_USER, `Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice`, registry.QUERY_VALUE)
 	if regErr != nil {
 		log.Errorf("Error reading UrlAssociations registry key: %v", regErr)
 		return false
 	}
-	s, _, err := k.GetStringValue("ProgId")
+	p, _, err := _p.GetStringValue("ProgId")
 	if err != nil {
 		log.Errorf("Could not read ProgId: %v", err)
 		return false
 	}
 
-	// This is an Edge programming ID, but it's not clear it's constant.
-	return s == "AppXq0fevzme2pys62n3e0fbqa7peapykr8v"
+	_s, regErr := registry.OpenKey(registry.CLASSES_ROOT, p, registry.QUERY_VALUE)
+	if regErr != nil {
+		log.Errorf("Error reading program registry key: %v", regErr)
+		return false
+	}
+	s, _, err := _s.GetStringValue("FriendlyTypeName")
+	if err != nil {
+		log.Errorf("Could not read FriendlyTypeName: %v", err)
+		return false
+	}
+
+	return strings.Contains(s, "MicrosoftEdge")
 }
